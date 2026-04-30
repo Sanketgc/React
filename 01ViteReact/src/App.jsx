9810cae97e10f420feb6df8dd55e7a5ee3dc232c
Sanket <sanketchanagond07@gmail.com>
2026-04-30 22:21:12 +0530

    Begining With Vite

---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+  <h1>Welcome to Vite | React</h1>
+  );
+}
+
+export default App
