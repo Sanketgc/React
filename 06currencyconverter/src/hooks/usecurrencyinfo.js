3eccd5a3f17e77d8709a22f6ced539a7ffbb3a60
Sanket <sanketchanagond07@gmail.com>
2026-05-11 22:04:49 +0530

    Custom hooks

--- 06currencyconverter/src/hooks/usecurrencyinfo.js
+++ 06currencyconverter/src/hooks/usecurrencyinfo.js
@@ -1,2 +1,19 @@
 import { useEffect, useState } from "react"
+Currencyinfo = process.env.CURRENCYINFO
+
+function useCurrency(currency){
+const [data, setdata] = useState({})
+
+    useEffect(() => {
+        fetch(Currencyinfo)
+            .then((res) => res.json())
+            .then((res) => setdata(res[currency]))
+            console.log(data);
+
+    }, [currency])  
+    console.log(data);
+    return data;
+}
+
+export default useCurrency;
 
