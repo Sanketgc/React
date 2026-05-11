1ab63a3d881915615f3f35dc7886456d01aba596
Sanket <sanketchanagond07@gmail.com>
2026-05-11 22:15:58 +0530

    learning custom hooks

--- 06currencyconverter/src/hooks/usecurrencyinfo.js
+++ 06currencyconverter/src/hooks/usecurrencyinfo.js
@@ -17,3 +17,26 @@ const [data, setdata] = useState({})
 
 export default useCurrency;
 
+
+
+import { useState, useEffect } from 'react';
+
+export default function StatusBar() {
+  const [isOnline, setIsOnline] = useState(true);
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+    function handleOffline() {
+      setIsOnline(false);
+    }
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+}
